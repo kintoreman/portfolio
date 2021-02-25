8f60966ea34224c61f6331500320afb830a6c877
unknown <dokkyou93@gmail.com>
2021-02-25 11:10:33 +0900

    解決した気がする

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,8 @@ import Menu from "./menu/menu";
 import Button from "./button/button";
 import Contact from "./menu/contact";
 import Me from "./me/me";
-import { useSpring, animated } from "react-spring";
+import { yellow } from "@material-ui/core/colors";
+// import { useSpring, animated } from "react-spring";
 
 //静的ファイルは基本的にはpublicにいれる！！
 //iconとかもpublicにいれる
@@ -15,50 +16,58 @@ const useStyles = makeStyles({
         display: "flex",
         justifyContent: "center",
         marginTop: "3%",
+        fontSize: "30px",
+        color: "black",
+        transition:"all 3s ease-out",
+        "&:hover":{
+            fontSize : "60px",
+            color : "yellow",
+        },
+        // fontSize: enter ? "60px" : "31px" ,
+        // color: enter ? "yellow" : "black",
     }
 })
 
 const Home = () => {
     const classes = useStyles();
-    const [enter, setEnter] = useState(false);
-    const spring = useSpring({
-        fontSize: enter ? "60px" : "31px" ,
-        color: enter ? "yellow" : "black"
-    });
+    // const enter = useState(false)
+    // // const spring = useSpring({
+    // //     fontSize: enter ? "60px" : "31px" ,
+    // //     color: enter ? "yellow" : "black"
+    // // });
 
     return (
         //titleに背景とコンポーネントMenuをいれたい
         <React.Fragment>
     
             <Menu />
-            <animated.div 
+            <div 
                 className={classes.h1}
-                style={spring}
-                onMouseEnter={e => setEnter(true)}
-                onMouseLeave={e => setEnter(false)}
+                // onMouseEnter={(e) => enter(true)}
+                // onMouseLeave={e => enter(false)}
             >
                 ---Profile---
-            </animated.div>
+            </div>
             <Me />
-            <animated.div 
+            {/* <animated.div 
                 className={classes.h1}
                 style={spring}
                 onMouseEnter={e => setEnter(true)}
                 onMouseLeave={e => setEnter(false)}
             >
                 ---Contact---
-            </animated.div>
+            </animated.div> */}
            
             <Contact />
             
-            <animated.div 
+            {/* <animated.div 
                 className={classes.h1}
                 style={spring}
                 onMouseEnter={e => setEnter(true)}
                 onMouseLeave={e => setEnter(false)}
             >
                 ---Skills---
-            </animated.div>
+            </animated.div> */}
 
             <Button />
 
